45cf952d66a137ee236353d3c3fb798a8c588a47
Felipe Huggler Luz <felipe@felipehuggler.com>
2014-10-03 19:26:07 -0300

    telas

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -155,3 +155,69 @@ $(document).ready(function(){
   $(document).on('click', '.dropdown-menu a', selectFormGroup);
 
 });
+
+
+function testAPI() {
+  FB.api('/me', function(response) {
+    console.log(response);
+    document.getElementById('status').innerHTML = '<a class="dropdown-toggle" data-toggle="dropdown">Olá, ' + response.name + '! <img src="http://graph.facebook.com/'+ response.id + '/picture"/ class="img-circle"/><b class="caret"></b></a><ul class="dropdown-menu"><li onclick="logout()">sair</li></ul>';
+  });
+};
+
+function logout(){
+  FB.logout(function(response){
+    statusChangeCallback(response);
+  })
+};
+
+// This is called with the results from from FB.getLoginStatus().
+  function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' + 'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = '';
+    }
+  };
+
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  };
+
+
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '1579397852288566',
+    xfbml      : true,
+    version    : 'v2.1'
+  });
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = '//connect.facebook.net/en_US/sdk.js';
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
