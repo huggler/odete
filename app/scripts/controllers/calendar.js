26d831fd0fdf1dd5ea3a7950c118310376843dd9
Felipe Huggler <felipe.luz@ideais.com.br>
2014-10-07 00:30:39 -0300

    inclusoes

---
+++ app/scripts/controllers/calendar.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularApp')
+  .controller('CalendarCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
