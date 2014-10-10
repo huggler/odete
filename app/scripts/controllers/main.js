90a5d681b438ded2ae1f8ff5ab390b5a3600b562
Felipe Huggler <felipe.luz@ideais.com.br>
2014-10-09 22:09:18 -0300

    icone odete

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,22 +20,41 @@ angular.module('angularApp')
     $scope.filters.data = new Date();
     $scope.filters.periodo = '';
 
-    $scope.filters.uf = '';
+    $scope.filters.estado = '';
     $scope.filters.cidade = '';
     $scope.filters.bairro = '';
 
+    $scope.filters.estados = '';
+    $scope.filters.cidades = '';
+    $scope.filters.bairros = '';
+
+
     $scope.updateResults = function(){
-      $http.get('back/index.php/pesquisar', { params : { data : $scope.filters }}).then(function(data){
+      $http.get('http://odete.felipehuggler.com/back/index.php/pesquisar', { params : { data : $scope.filters }}).then(function(data){
         $scope.result = data.data;
       });
     };
 
-    $scope.getCidades = function(){
+    $scope.getEstado = function(){
+      $http.get('http://odete.felipehuggler.com/back/index.php/pesquisar/estado', { params : { data : $scope.filters }}).then(function(data){
+        $scope.filters.estados = data.data;
+      });
+    };
+
+    $scope.getCidade = function(){
       $http.get('http://odete.felipehuggler.com/back/index.php/pesquisar/cidade', { params : { data : $scope.filters }}).then(function(data){
-        $scope.filters.cidade = data.data;
+        $scope.filters.cidades = data.data;
+      });
+    };
+
+    $scope.getBairro = function(){
+      $http.get('http://odete.felipehuggler.com/back/index.php/pesquisar/bairro', { params : { data : $scope.filters }}).then(function(data){
+        $scope.filters.bairros = data.data;
       });
     };
 
     $scope.updateResults();
-    $scope.getCidades();
+    $scope.getEstado();
+    $scope.getCidade();
+    $scope.getBairro();
   });
