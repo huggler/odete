f2d7f0b5620d453c55ac5ac6ac535779da6ca64a
Felipe Huggler <felipe.luz@ideais.com.br>
2014-10-15 01:18:04 -0300

    alteracoes na view main

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,6 +7,8 @@
  * # MainCtrl
  * Controller of the angularApp
  */
+var geocoder;
+
 angular.module('angularApp')
   .controller('MainCtrl', function ($scope, $http) {
 
@@ -53,8 +55,105 @@ angular.module('angularApp')
       });
     };
 
+   $scope.getMarker = function(){
+      if (navigator.geolocation) {
+          geocoder = new google.maps.Geocoder();
+          navigator.geolocation.getCurrentPosition($scope.showPosition, $scope.handleError);
+      }
+    };
+
+    $scope.showPosition = function(position) {
+        $scope.showAddress(position);
+    };
+
+    $scope.showAddress = function(position){
+        var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+        var objCep = {
+          numero : 'street_number',
+          cidade : 'locality',
+          endereco : 'route',
+          zip : 'postal_code',
+          estado : 'administrative_area_level_1',
+          bairro : 'neighborhood',
+          pais : 'country'
+        };
+ 
+        geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+            
+            if (status === google.maps.GeocoderStatus.OK) {
+                
+                if (results[1]) {
+
+                  var addressList = results[0].address_components;
+                  var addressListLen = addressList.length;
+                  var address = '';
+                  var typeAdressList = '';
+                  var typeAdressListLen = '';
+                  var typeAdress = '';
+
+                    for (var i = 0; i < addressListLen; i++) {
+
+                      address = addressList[i];
+                      typeAdressList = address.types;
+                      typeAdressListLen = typeAdressList.length;
+
+                        for (var b = 0; b < typeAdressListLen; b++) {
+
+                          typeAdress = typeAdressList[b];
+
+                            if (typeAdress === objCep.numero) {
+                                objCep.numero = address.short_name;
+                            }
+                            if (typeAdress === objCep.cidade) {
+                                objCep.cidade = address.short_name;
+                            }
+                            if (typeAdress === objCep.endereco) {
+                                objCep.endereco = address.short_name;
+                            }
+                            if (typeAdress === objCep.zip) {
+                                objCep.zip = address.short_name;
+                            }
+                            if (typeAdress === objCep.estado) {
+                                objCep.estado = address.short_name;
+                            }
+                            if (typeAdress === objCep.bairro) {
+                                objCep.bairro = address.short_name;
+                            }
+                            if (typeAdress === objCep.pais) {
+                                objCep.pais = address.short_name;
+                            }
+                         }
+                     }
+
+                     $scope.cep = objCep;
+                     $scope.$apply();
+                 }
+            }
+        });
+    };
+ 
+    $scope.handleError = function(error) {
+        switch(error.code){
+            case error.PERMISSION_DENIED:
+                $scope.errorMsg = ('Usuário negou o pedido de Geolocalização.');
+            break;
+            case error.POSITION_UNAVAILABLE:
+                $scope.errorMsg = ('Informações sobre a localização está indisponível.');
+            break;
+            case error.TIMEOUT:
+                $scope.errorMsg = ('O pedido para obter a localização do usuário expirou.');
+            break;
+            case error.UNKNOWN_ERROR:
+                $scope.errorMsg = ('Ocorreu um erro desconhecido.');
+            break;
+         }
+    }; 
+
     $scope.updateResults();
     $scope.getEstado();
     $scope.getCidade();
     $scope.getBairro();
+
+    $scope.getMarker();
   });
